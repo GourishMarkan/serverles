3013a6abe9873a275c539c7dadf3722f30ca36a5
Gourish Markan <142171408+GourishMarkan@users.noreply.github.com>
2024-05-31 21:10:40 +0530

    first commit

---
+++ hono-app1/src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+// Getting inputs from user
+app.get("/", async (c) => {
+  // const body = await c.req.json();
+  // console.log(body);
+  // console.log(c.req.header("Authorization"));
+  // console.log(c.req.query("param"));
+
+  return c.text("Hello Hono!");
+});
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text("Hello Hono!");
+});
+
+export default app;
